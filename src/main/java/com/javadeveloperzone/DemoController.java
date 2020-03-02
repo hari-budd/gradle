8fb3aa93de8db6c95c5af1d55781c98fc4aaa444
hari-budd <61681776+hari-budd@users.noreply.github.com>
2020-03-02 15:23:11 +0530

    Update DemoController.java

--- src/main/java/com/javadeveloperzone/DemoController.java
+++ src/main/java/com/javadeveloperzone/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @RequestMapping(value = "hello")
     public String hello(){
-        return "Spring boot Gradle Example";
+        return "Hello Honeywell. This is Spring boot Gradle Sample Project for POC";
     }
 }
